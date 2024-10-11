9c5a35829d972b673493d1e4b8663aaedb9d2470
Zeeshan Azhar <154502262+zetro-nayatel@users.noreply.github.com>
2024-10-11 19:28:31 +0500

    display users in tableview

--- moya-integration/View/UsersViewController.swift
+++ moya-integration/View/UsersViewController.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
-class UsersViewController: UIViewController {
+class UsersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // MARK: Variables
     private let networkManager: NetworkManager = NetworkManager()
     private var users: [User] = []
     
     // MARK: IBOutlets
-
+    @IBOutlet weak var tableview: UITableView!
     
     // MARK: - View lifecycle
     
@@ -23,6 +23,8 @@ class UsersViewController: UIViewController {
         super.viewDidLoad()
         
         self.navigationItem.title = "Users"
+        tableview.delegate = self
+        tableview.dataSource = self
         
         loadUsers();
 
@@ -38,11 +40,28 @@ class UsersViewController: UIViewController {
                 print("this is the response")
                 print(userResponse);
                 self?.users = userResponse
+                self?.tableview.reloadData();
             case .failure(let error):
                 print(error.localizedDescription)
             }
         }
     }
+    
+    
+    // MARK: Table View
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ??
+        UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel!.text = users[indexPath.row].title
+        return cell
+    }
+    
+    
     
 
 }
