2a3ceef56d908ed54038cd7dc9d983a63e77cc72
Zeeshan Azhar <154502262+zetro-nayatel@users.noreply.github.com>
2024-10-11 19:13:28 +0500

    add fetch users API

--- moya-integration/View/UsersViewController.swift
+++ moya-integration/View/UsersViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class UsersViewController: UIViewController {
+    
+    // MARK: Variables
+    private let networkManager: NetworkManager = NetworkManager()
+    private var users: [User] = []
+    
+    // MARK: IBOutlets
 
+    
+    // MARK: - View lifecycle
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.title = "Users"
+        
+        loadUsers();
 
+    }
+    
+    // MARK: API Call
+    
+    private func loadUsers() {
+        print("API calling")
+        networkManager.fetchUsers { [weak self] result in
+            switch result {
+            case .success(let userResponse):
+                print("this is the response")
+                print(userResponse);
+                self?.users = userResponse
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
 
